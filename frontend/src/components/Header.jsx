92966400a80f6321cb1a7a65a5cb4d39d1ed5c6f
afsalboot <142302245+afsalboot@users.noreply.github.com>
2025-06-27 19:10:21 +0530

    Completed

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import NavBar from './NavBar'
+import { motion } from "motion/react"
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
+        <NavBar/>
+        <motion.div  
+        initial={{opacity:0, y:100}}
+        transition={{duration: 1.5}}
+        whileInView={{opacity:1, y:0}}
+        viewport={{once:true}}
+        className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block mx-w-3xl font-semibold pt-20 '>Explore homes that fit your dreams</h2>
+            <div className='space-x-6 mt-16'>
+                <a href="#Projects" className='border border-white py-3 px-8 rounded'>Projects</a>
+                <a href="#Contacts" className='bg-blue-500 py-3 px-8 rounded' id='Contact'>Contact Us</a>
+            </div>
+        </motion.div>
+    </div>
+  )
+}
+
+export default Header
